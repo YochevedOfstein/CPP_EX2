ed3d3eaa61a14263a6f32c4444fb9a5faaceecf8
Yocheved <yocheved@davidowitz.net>
2024-05-27 16:59:33 +0300

    readme and tests

--- Test.cpp
+++ Test.cpp
@@ -541,7 +541,326 @@ TEST_CASE("Test negative cycles in an undirected graph")
     g.loadGraph(graph);
     CHECK(ariel::Algorithms::negativeCycle(g) == "Negative cycle: 3 -> 1 -> 3");
 
+}
 
+///////////////////////////////////////// Graph operators tests /////////////////////////////////////////
+
+TEST_CASE("Test graph addition")
+{
+    ariel::Graph g1;
+    vector<vector<int>> graph = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g1.loadGraph(graph);
+
+    ariel::Graph g2;
+    vector<vector<int>> graph2 = {
+        {0, 1, 1},
+        {1, 0, 1},
+        {1, 1, 0}};
+    g2.loadGraph(graph2);
+
+    ariel::Graph g3 = g1 + g2;
+
+    ariel::Graph g4;
+    vector<vector<int>> expected = {
+        {0, 2, 1},
+        {2, 0, 2},
+        {1, 2, 0}};
+    g4.loadGraph(expected);
+
+    CHECK((g3.getGraph() == expected) == true);
+
+    SUBCASE("Test graph addition with negative weights")
+    {
+        ariel::Graph g5;
+        vector<vector<int>> graph5 = {
+            {0, -3, 0},
+            {-1, 0, -1},
+            {0, -3, 0}};
+        g5.loadGraph(graph5);
+
+        ariel::Graph g6 = g1 + g5;
+
+        ariel::Graph g7;
+        vector<vector<int>> expected2 = {
+            {0, -2, 0},
+            {0, 0, 0},
+            {0, -2, 0}};
+        g7.loadGraph(expected2);
+
+        CHECK((g6.getGraph() == expected2) == true);
+    }
+
+    SUBCASE("Test graph addition assignment operator")
+    {
+        g1 += g2;
+        CHECK((g1.getGraph() == g4.getGraph()) == true);
+    }
+
+    SUBCASE("Test unary plus operator")
+    {
+        ariel::Graph g8 = +g1;
+        CHECK((g8.getGraph() == g1.getGraph()) == true);
+    }
+}
+
+TEST_CASE("Test graph subtraction")
+{
+    ariel::Graph g1;
+    vector<vector<int>> graph = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g1.loadGraph(graph);
+
+    ariel::Graph g2;
+    vector<vector<int>> graph2 = {
+        {0, 1, 1},
+        {1, 0, 1},
+        {1, 1, 0}};
+    g2.loadGraph(graph2);
+
+    ariel::Graph g3 = g1 - g2;
+
+    ariel::Graph g4;
+    vector<vector<int>> expected = {
+        {0, 0, -1},
+        {0, 0, 0},
+        {-1, 0, 0}};
+    g4.loadGraph(expected);
+
+    CHECK((g3.getGraph() == expected) == true);
+
+    SUBCASE("Test graph subtraction with negative weights")
+    {
+    ariel::Graph g5;
+    vector<vector<int>> graph5 = {
+        {0, -3, 0},
+        {-1, 0, -1},
+        {0, -3, 0}};
+    g5.loadGraph(graph5);
+
+    ariel::Graph g6 = g1 - g5;
+
+    ariel::Graph g7;
+    vector<vector<int>> expected2 = {
+        {0, 4, 0},
+        {2, 0, 2},
+        {0, 4, 0}};
+
+    g7.loadGraph(expected2);
+
+    CHECK((g6.getGraph() == expected2) == true);
+    }
+
+    SUBCASE("Test graph subtraction assignment operator")
+    {
+        g1 -= g2;
+        CHECK((g1.getGraph() == expected) == true);
+        CHECK((g1.getGraph() != g3.getGraph()) == false);
+    }
+
+    SUBCASE("Test unary minus operator")
+    {
+        ariel::Graph g8 = -g1;
+        CHECK((g8.getGraph() == g1.getGraph()) == false);
+    }
+}
+
+
+TEST_CASE("Test graph multiplication")
+{
+    ariel::Graph g1;
+    vector<vector<int>> graph = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g1.loadGraph(graph);
+
+    ariel::Graph g2;
+    vector<vector<int>> graph2 = {
+        {0, 1, 1},
+        {1, 0, 1},
+        {1, 1, 0}};
+    g2.loadGraph(graph2);
+
+    ariel::Graph g3 = g1 * g2;
+
+    ariel::Graph g4;
+    vector<vector<int>> expected = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g4.loadGraph(expected);
+
+
+
+    CHECK((g3.getGraph() == expected) == true);
+
+    SUBCASE("Test graph multiplication assignment operator")
+    {
+        g1 *= g2;
+        CHECK((g1.getGraph() == g4.getGraph()) == true);
+    }
+
+    SUBCASE("Test multiplication by scalar")
+    {
+        ariel::Graph g5 = g1 * 2;
+        ariel::Graph g6;
+        vector<vector<int>> expected2 = {
+            {0, 2, 0},
+            {2, 0, 2},
+            {0, 2, 0}};
+        g6.loadGraph(expected2);
+
+        CHECK((g5.getGraph() == expected2) == true);
+    }
+}
+
+TEST_CASE("Test increment and decrement operators"){
+
+    ariel::Graph g1;
+    vector<vector<int>> graph = {
+        {0, 2, 3},
+        {2, 0, 4},
+        {3, 4, 0}};
+    g1.loadGraph(graph);
+
+    ariel::Graph g2 = g1;
+
+    g1++;
+    CHECK((g1.getGraph() != g2.getGraph()) == true); 
+
+    g1 = g2;
+
+    ++g1;
+    CHECK((g1.getGraph() == g2.getGraph()) == false); 
+
+    g1 = g2;
+
+    g1--;
+    CHECK((g1.getGraph() != g2.getGraph()) == true); 
+
+    g1 = g2;
+
+    --g1;
+    CHECK((g1.getGraph() == g2.getGraph()) == false); 
+}
+
+TEST_CASE("Test comparison operators")
+{
+    ariel::Graph g1;
+    vector<vector<int>> graph = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 2, 0}};
+    g1.loadGraph(graph);
+
+    ariel::Graph g2;
+    vector<vector<int>> graph2 = {
+        {0, 1, 1},
+        {1, 0, 2},
+        {1, 2, 0}};
+    g2.loadGraph(graph2);
+
+    CHECK((g1 == g2) == false);
+    CHECK((g1 != g2) == true);
+    CHECK((g1 < g2) == true);
+    CHECK((g1 > g2) == false);
+    CHECK((g1 <= g2) == true);
+    CHECK((g1 >= g2) == false);
+
+    ariel::Graph g3;
+    vector<vector<int>> graph3 = {
+        {0, 1, 0, 3},
+        {1, 0, 1, 1},
+        {7, 1, 0, 2},
+        {10, 1, 2, 0}};
+    g3.loadGraph(graph3);
+
+    ariel::Graph g4;
+    vector<vector<int>> graph4 = {
+        {0, -1, 0, 0},
+        {1, 0, -14, 4},
+        {7, 1, 0, -12},
+        {-3, 1, 2, 0}};
+    g4.loadGraph(graph4);
+
+    CHECK((g3 == g4) == false);
+    CHECK((g3 != g4) == true);
+    CHECK((g3 < g4) == false);
+    CHECK((g3 > g4) == true);
+    CHECK((g3 <= g4) == false);
+    CHECK((g3 >= g4) == true);
+
+    ariel::Graph g5;
+    vector<vector<int>> graph5 = {
+        {0, 1},
+        {1, 0}};
+    g5.loadGraph(graph5);
+
+    CHECK((g5 == g5) == true);
+    CHECK((g5 > g4) == false);//
+    CHECK((g5 < g4) == true);//
+    CHECK((g5 < g2) == true);//
+
+}
+
+TEST_CASE("Test invalid operations"){
+    ariel::Graph g1;
+    vector<vector<int>> graph = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g1.loadGraph(graph);
+
+    ariel::Graph g2;
+    vector<vector<int>> graph2 = {
+        {0, 1},
+        {1, 0}};
+    g2.loadGraph(graph2);
+
+    ariel::Graph g3;
+    vector<vector<int>> graph3 = {
+        {0, 1, 0, 3},
+        {1, 0, 1, 0},
+        {7, 8, 0, 2},
+        {10, 0, 2, 0}};
+    g3.loadGraph(graph3);
+
+    CHECK_THROWS(g1 + g2);
+    CHECK_THROWS(g1 - g2);
+    CHECK_THROWS(g1 * g2);
+
+    CHECK_THROWS(g1 + g3);
+    CHECK_THROWS(g1 - g3);
+    CHECK_THROWS(g1 * g3);
+}
+
+TEST_CASE("Test algorithms and operations"){
+
+    ariel::Graph g1;
+    vector<vector<int>> graph1 = {
+        {0, 1, 1},
+        {1, 0, 1},
+        {1, 1, 0}};
+    g1.loadGraph(graph1);
+
+    ariel::Graph g2;
+    vector<vector<int>> graph2 = {
+        {0, 2, 1},
+        {2, 0, 2},
+        {1, 2, 0}};
+    g2.loadGraph(graph2);
+
+    CHECK(ariel::Algorithms::isConnected(g1 + g2) == true);
+    CHECK(ariel::Algorithms::isContainsCycle(g2 * -1 + g1) == "-1");
+    CHECK(ariel::Algorithms::isBipartite(g1 + g2) == "The graph is not bipartite");
+    CHECK(ariel::Algorithms::shortestPath(g1 + g2, 0, 2) == "0 -> 2");
+    CHECK(ariel::Algorithms::shortestPath(g1 - g2, 0, 2) == "Graph contains negative cycle");
+    CHECK(ariel::Algorithms::negativeCycle(g1 + g2) == "No negative cycle found");
 }
 
 
